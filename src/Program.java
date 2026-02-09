73688631df9b13c4b754730f2865c5d65c025a1c
Arthur Lucas <lucasarthur493@gmail.com>
2026-02-09 20:57:11 -0300

    Seller entity

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,15 @@
 import model.entities.Departament;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String args[]){
         Departament d = new Departament(1, "Books");
-        System.out.println(d);
+
+        Seller seller = new Seller(21, "Jose", "josefino@gmail.com", new Date(), 1000.0, d);
+
+        System.out.println(seller);
+
     }
 }
