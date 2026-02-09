9aa1748a0ce991d3b05f1364e8c07bd36d0cc5af
Arthur Lucas <lucasarthur493@gmail.com>
2026-02-09 20:41:59 -0300

    Departament entity class

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,8 @@
+import model.entities.Departament;
+
 public class Program {
     public static void main(String args[]){
-
+        Departament d = new Departament(1, "Books");
+        System.out.println(d);
     }
 }
---
+++ src/model/entities/Departament.java
@@ -0,0 +1,51 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departament implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+
+    public Departament(){
+    }
+
+    public Departament(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Departament that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public String toString(){
+        return "Departament [id =" + id + ", name =" + name + "]";
+    }
+}
